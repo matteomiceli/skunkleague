2147663779523b884494f4d50f4f4c0d6d25f092
Matteo Miceli <omatteomiceli@gmail.com>
2022-04-27 03:30:22 -0700

    db connected

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"log"
+
+	db "github.com/matteomiceli/skunkleague/src/database"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main () {
+	// abstract this to a server init func
+	err := godotenv.Load(".env")
+	if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+	db.Init()
+
 	r := gin.Default()
+
 	r.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(200, map[string]string{"hi": "hi"})
 	})
+	
 	r.Run()
 }
---
+++ src/database/db.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func Init() {
+	client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+}
